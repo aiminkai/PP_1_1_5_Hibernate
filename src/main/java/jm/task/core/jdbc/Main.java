68c39b907b529dcd6cbaa2c8c70f6651e1821b30
aiminkai <m.a.azarka@gmail.com>
2022-11-10 07:43:11 +0300

    edit object creation in Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,13 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserServiceImpl userDao = new UserServiceImpl();
 
         userDao.createUsersTable();
 
